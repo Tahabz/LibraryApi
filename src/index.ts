e228d95475455545cd60424bb6777b0ecd1883f9
Tahabz <etahabaz@gmail.com>
2021-11-17 10:32:07 +0100

    add start() in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import { connect } from './db';
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,15 @@ app.get('/', (req, res) => {
   return res.end('hello world');
 });
 
-app.listen(process.env.PORT, () => {
-  console.log('server up and running at port ' + process.env.PORT);
-});
+const start = async () => {
+  try {
+    await connect();
+    app.listen(process.env.PORT, () => {
+      console.log(`Server up and running on port ${process.env.PORT} !`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+start();
