1d5f26944ace245534dcde612c908dca1a69c231
Tahabz <etahabaz@gmail.com>
2021-11-20 17:15:27 +0100

    add reservation model and controller

---
+++ src/controllers/reservation.ts
@@ -0,0 +1,4 @@
+import reservation from '../services/reservation';
+import controller from '../shared/controller';
+
+export default controller(reservation);
---
+++ src/services/reservation.ts
@@ -0,0 +1,4 @@
+import reservationModel from '../models/Reservation';
+import crud from '../shared/crud';
+
+export default crud(reservationModel);
