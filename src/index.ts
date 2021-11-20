6dcb4a5aff14489536d6edc1da16e21b47f0bd5f
Tahabz <etahabaz@gmail.com>
2021-11-20 20:09:02 +0100

    use the error middleware in the index app

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { connect } from './db';
+import adminRouter from './routes/admin';
+import error from './middlewares/error';
 
 dotenv.config();
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   return res.end('hello world');
 });
 
+app.use('/admin', adminRouter);
+
+app.use(error);
 const start = async () => {
   try {
     await connect();
