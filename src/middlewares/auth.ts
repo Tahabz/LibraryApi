50341291bc165a574b36753308c862515680e23f
Tahabz <etahabaz@gmail.com>
2021-11-20 20:08:43 +0100

    add admin auth logic

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,10 +1,29 @@
 import jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
 import { NextFunction, Request, Response } from 'express';
+import admin from '../services/admin';
 
 dotenv.config();
 
-export default (req: Request, res: Response, next: NextFunction) => {
+export const adminAUth = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    let token;
+    if (Object.prototype.hasOwnProperty.call(req, 'cookies')) token = req.cookies['JWT'];
+    if (!token) {
+      token = req.headers['authorization'].split(' ')[1];
+    }
+    const res = jwt.verify(token, process.env.JWT_SECRET);
+    const admn = await admin.getOne({ username: res?.username })()();
+
+    if (!admn) throw new Error('not allowed');
+    // req.user = admn;
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const userAuth = (req: Request, res: Response, next: NextFunction) => {
   try {
     let token = req.cookies['JWT'];
     if (!token) {
