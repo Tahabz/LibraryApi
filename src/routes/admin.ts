5bbfe677b4c2706d4c6efe9932196ff442a155a2
Tahabz <etahabaz@gmail.com>
2021-11-20 17:12:52 +0100

    add more admin routes

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { login } from '../controllers/auth';
+import book from '../controllers/book';
 import auth from '../middlewares/auth';
 
 const adminRouter = Router();
@@ -7,3 +8,9 @@ const adminRouter = Router();
 adminRouter.post('/login', login);
 adminRouter.use(auth);
 adminRouter.get('/users');
+adminRouter.get('/books', book.getAll);
+adminRouter.get('/books/:id', async (req, res, next) => {
+  const select = book.getOne({ _id: req.params.id });
+  const exec = select();
+  return await exec(req, res, next);
+});
