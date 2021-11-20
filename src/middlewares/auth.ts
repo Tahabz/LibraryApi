fd0c60b30a9b0e8a541e317363ba06f8b42c6f6b
Tahabz <etahabaz@gmail.com>
2021-11-20 20:27:30 +0100

    add user auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
 import { NextFunction, Request, Response } from 'express';
 import admin from '../services/admin';
+import user from '../services/user';
 
 dotenv.config();
 
@@ -23,13 +24,18 @@ export const adminAUth = async (req: Request, res: Response, next: NextFunction)
   }
 };
 
-export const userAuth = (req: Request, res: Response, next: NextFunction) => {
+export const userAuth = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    let token = req.cookies['JWT'];
+    let token;
+    if (Object.prototype.hasOwnProperty.call(req, 'cookies')) token = req.cookies['JWT'];
     if (!token) {
       token = req.headers['authorization'].split(' ')[1];
     }
-    jwt.verify(token, process.env.JWT_SECRET);
+    const res = jwt.verify(token, process.env.JWT_SECRET);
+    const admn = await user.getOne({ username: res?.username })()();
+
+    if (!admn) throw new Error('not allowed');
+    // req.user = admn;
     next();
   } catch (e) {
     next(e);
