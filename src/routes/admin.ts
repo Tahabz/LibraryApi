60189855ded8223ffb0f39201e54e7137bb1e51b
Tahabz <etahabaz@gmail.com>
2021-11-20 18:09:59 +0100

    add more admin routes

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -7,14 +7,18 @@ import auth from '../middlewares/auth';
 const adminRouter = Router();
 
 adminRouter.post('/login', login);
+
 adminRouter.use(auth);
+
 adminRouter.get('/users');
-adminRouter.get('/books', book.getAll);
+
 adminRouter.get('/books/:id', async (req, res, next) => {
   const select = book.getOne({ _id: req.params.id });
   const exec = select();
   return await exec(req, res, next);
 });
+adminRouter.route('/books').post(book.createOne).get(book.getAll).delete(book.deleteOne).put(book.updateOne);
+
 adminRouter.get('/reservations', reservation.getAll);
 adminRouter.get('/reservations/:id', async (req, res, next) => {
   const select = reservation.getOne({ _id: req.params.id });
