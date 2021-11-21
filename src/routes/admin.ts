6e41ef17209d6b61a9c0a2be3416343783067b87
Tahabz <etahabaz@gmail.com>
2021-11-21 16:29:09 +0100

    get books by req.query

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -3,24 +3,37 @@ import admin from '../controllers/admin';
 import { adminLogin } from '../controllers/auth';
 import book from '../controllers/book';
 import reservation from '../controllers/reservation';
+import user from '../controllers/user';
 import { adminAUth } from '../middlewares/auth';
 
 const adminRouter = Router();
 
 adminRouter.post('/login', adminLogin);
 
-adminRouter.use(adminAUth);
-
 adminRouter.post('/register', admin.createOne);
 
-adminRouter.get('/users', (req, res) => res.end('hello world'));
+adminRouter.use(adminAUth);
+
+adminRouter.get('/users', user.getAll);
 
 adminRouter.get('/books/:id', async (req, res, next) => {
   const select = book.getOne({ _id: req.params.id });
   const exec = select();
   return await exec(req, res, next);
 });
-adminRouter.route('/books').post(book.createOne).get(book.getAll).delete(book.deleteOne).put(book.updateOne);
+adminRouter
+  .route('/books')
+  .post(book.createOne)
+  .get(async (req, res, next) => {
+    if (Object.keys(req.query).length != 0) {
+      const select = book.getMany({ ...req.query });
+      const exec = select();
+      return await exec(req, res, next);
+    }
+    return await book.getAll(req, res, next);
+  })
+  .delete(book.deleteOne)
+  .put(book.updateOne);
 
 adminRouter.get('/reservations', reservation.getAll);
 adminRouter.get('/reservations/:id', async (req, res, next) => {
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,27 @@
 import { Router } from 'express';
 import * as dotenv from 'dotenv';
 import { userLogin } from '../controllers/auth';
+import book from '../controllers/book';
 
 dotenv.config();
 
 const userRouter = Router();
 
 userRouter.post('/login', userLogin);
+
+userRouter.get('/books/:id', async (req, res, next) => {
+  const select = book.getOne({ _id: req.params.id });
+  const exec = select();
+  return await exec(req, res, next);
+});
+
+userRouter.get('/books', async (req, res, next) => {
+  if (Object.keys(req.query).length != 0) {
+    const select = book.getMany({ ...req.query });
+    const exec = select();
+    return await exec(req, res, next);
+  }
+  return await book.getAll(req, res, next);
+});
+
 export default userRouter;
