4d62d3b3ce65d07e9b635947792df9eab2ecd007
Tahabz <etahabaz@gmail.com>
2021-11-22 18:32:29 +0100

    remove the unnecessary extend

--- src/shared/crud.ts
+++ src/shared/crud.ts
@@ -1,21 +1,21 @@
-import { AnyKeys, AnyObject, Document, FilterQuery, Model, PopulateOptions, QueryOptions, UpdateQuery } from 'mongoose';
+import { AnyKeys, AnyObject, FilterQuery, Model, PopulateOptions, QueryOptions, UpdateQuery } from 'mongoose';
 
 const getOne =
-  <T extends Document>(model: Model<T>) =>
+  <T>(model: Model<T>) =>
   (filter: FilterQuery<T>) =>
   (arg?: object | string | Array<string>) =>
   async () =>
     await model.findOne(filter).select(arg).lean().exec();
 
 const getMany =
-  <T extends Document>(model: Model<T>) =>
+  <T>(model: Model<T>) =>
   (filter: FilterQuery<T>) =>
   (arg?: object | string | Array<string>) =>
   async () =>
     await model.find(filter).select(arg).lean().exec();
 
 const getOneAndPopulate =
-  <T extends Document>(model: Model<T>) =>
+  <T>(model: Model<T>) =>
   (filter: FilterQuery<T>) =>
   (populateOptions: PopulateOptions) =>
   (arg?: object | string | Array<string>) =>
@@ -23,18 +23,18 @@ const getOneAndPopulate =
     await model.findOne(filter).select(arg).lean().populate(populateOptions).exec();
 
 const getAll =
-  <T extends Document>(model: Model<T>) =>
+  <T>(model: Model<T>) =>
   async () =>
     await model.find({}).lean().exec();
 
 const createOne =
-  <T extends Document>(model: Model<T>) =>
+  <T>(model: Model<T>) =>
   (data: AnyObject | AnyKeys<T>) =>
   async () =>
     await model.create(data);
 
 const updateOne =
-  <T extends Document>(model: Model<T>) =>
+  <T>(model: Model<T>) =>
   (filter: FilterQuery<T>) =>
   (data: UpdateQuery<T>) =>
   (options: QueryOptions = {}) =>
@@ -42,12 +42,12 @@ const updateOne =
     await model.findOneAndUpdate(filter, data, options).lean().exec();
 
 const deleteOne =
-  <T extends Document>(model: Model<T>) =>
+  <T>(model: Model<T>) =>
   (filter: FilterQuery<T>) =>
   async () =>
     await model.findOneAndDelete(filter).lean().exec();
 
-export default <T extends Document>(model: Model<T>) => ({
+export default <T>(model: Model<T>) => ({
   getOne: getOne(model),
   getMany: getMany(model),
   getOneAndPopulate: getOneAndPopulate(model),
