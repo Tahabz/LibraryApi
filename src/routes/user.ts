b7bed558d3b9c46dd609a0dfc2a1a41cfe99d130
Tahabz <etahabaz@gmail.com>
2021-11-26 16:16:00 +0100

    use userAuth middleware and add post and delete handlers in users router

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import * as dotenv from 'dotenv';
 import { userLogin } from '../controllers/auth';
 import sharedRouter from '../shared/router';
+import { userAuth } from '../middlewares/auth';
+import reservation from '../controllers/reservation';
 
 dotenv.config();
 
@@ -9,6 +11,10 @@ const userRouter = Router();
 
 userRouter.post('/login', userLogin);
 
+userRouter.use(userAuth);
+
 userRouter.use(sharedRouter);
+
+userRouter.route('/reservations').post(reservation.createOne).delete(reservation.deleteOne);
 
 export default userRouter;
